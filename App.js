8658c1bc73447fa34e90c48eefba5389308819b5
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-03-12 00:38:16 -0500

    connect and dispatch

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import MyApp from './src/MyApp';
+import reducer from './modules/reducer'; 
 
 const initState = {
   myCounter: 0
 }
+
+const store = createStore(reducer);
+
+const reducer = (state = initState, action) => {
+  
+  switch (action.type) {
+    case 'INC_COUNTER':
+      return { myCounter: state.myCounter + 1 };
+    case 'DEC_COUNTER': 
+      if (state.myCounter > 1) {
+        return { myCounter: state.myCounter - 1 }
+      } else {
+        return state;
+      }
+  }
+  return state;
+};
 
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <MyApp />
+      </Provider>
     );
   }
   
---
+++ src/MyApp.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+
+
+
+
+ class MyApp extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <TouchableOpacity>
+            <Text>Increase</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Decrease</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+  
+}
+
+function mapStateToProps(state) {
+  return {
+    myCounter: state.myCounter
+  };
+}
+function mapDispatchToProp(dispatch) {
+  return {
+    incCounter: () => dispatch({
+      type: 'INC_COUNTER'
+    }),
+    decCounter: () => dispatch({
+      type: 'DEC_COUNTER'
+    })
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProp)(MyApp)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
